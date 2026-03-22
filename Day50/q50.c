d21775f74cf508ab47d9fc3a03ea13211c38f69c
parasbhall_590029319 <parasbhall_590029319@users.noreply.github.com>
2026-03-22 12:00:00 +0530

    parasbhall_590029319

---
+++ Day50/q50.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int x) {
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = x;
+    n->left = n->right = NULL;
+    return n;
+}
+
+struct Node* insert(struct Node* root, int x) {
+    if (root == NULL) return createNode(x);
+    if (x < root->data)
+        root->left = insert(root->left, x);
+    else
+        root->right = insert(root->right, x);
+    return root;
+}
+
+int search(struct Node* root, int key) {
+    if (root == NULL) return 0;
+    if (root->data == key) return 1;
+    if (key < root->data) return search(root->left, key);
+    return search(root->right, key);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    if (search(root, key))
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
