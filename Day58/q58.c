6c7475451ead133fbc8b61961f2a5fddedd0b1c1
parasbhall_590029319 <parasbhall_590029319@users.noreply.github.com>
2026-03-30 12:00:00 +0530

    parasbhall_590029319

---
+++ Day58/q58.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int search(int inorder[], int start, int end, int value) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == value) return i;
+    }
+    return -1;
+}
+
+Node* build(int preorder[], int inorder[], int start, int end, int* preIndex) {
+    if (start > end) return NULL;
+
+    Node* root = newNode(preorder[*preIndex]);
+    (*preIndex)++;
+
+    if (start == end) return root;
+
+    int inIndex = search(inorder, start, end, root->data);
+
+    root->left = build(preorder, inorder, start, inIndex - 1, preIndex);
+    root->right = build(preorder, inorder, inIndex + 1, end, preIndex);
+
+    return root;
+}
+
+void postorder(Node* root) {
+    if (!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* preorder = (int*)malloc(n * sizeof(int));
+    int* inorderArr = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) scanf("%d", &preorder[i]);
+    for (int i = 0; i < n; i++) scanf("%d", &inorderArr[i]);
+
+    int preIndex = 0;
+    Node* root = build(preorder, inorderArr, 0, n - 1, &preIndex);
+
+    postorder(root);
+
+    free(preorder);
+    free(inorderArr);
+    return 0;
+}
