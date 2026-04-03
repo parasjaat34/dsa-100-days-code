caa34dfb35d1daeb9b09491718b76b7a5ab567c9
parasbhall_590029319 <parasbhall_590029319@users.noreply.github.com>
2026-04-03 12:00:00 +0530

    parasbhall_590029319

---
+++ Day62/q62.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+Node* createNode(int v) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    Node* adj[n];
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d:", i);
+        Node* temp = adj[i];
+        while (temp) {
+            printf(" %d", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
